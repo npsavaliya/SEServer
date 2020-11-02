073152fb468d3545bd43abcb3678f0230d81084b
npsavaliya <nirav.savalia1234@gmail.com>
2020-11-02 20:25:50 +0530

    changes in data

---
+++ src/Fixtures/Strings.js
@@ -0,0 +1,35 @@
+
+export const FlourmillWithMidRangeStone = {
+    name: { fieldName: "name", uiName: "Name" },
+    grindingStone: { fieldName: "grindingStone", uiName: "Grinding Stone Thickness" },
+    electricMotorCapacity: { fieldName: "electricMotorCapacity", uiName: "Electric Motor Capacity" },
+    powerConsumption: { fieldName: "powerConsumption", uiName: "Power Consumption Per Hour" },
+    vBeltSize: { fieldName: "vBeltSize", uiName: "V-Belt Size" },
+    shaftSize: { fieldName: "shaftSize", uiName: "En-8 Shaft Diameter" },
+    bearingSize: { fieldName: "bearingSize", uiName: "Ball Bearing" },
+    grindingCapacity: { fieldName: "grindingCapacity", uiName: "Grinding Capacity Per Hour (on 230 v)" },
+}
+
+export const FlourmillWithLargeRangeStone = {
+    name: { fieldName: "name", uiName: "Name" },
+    grindingStone: { fieldName: "grindingStone", uiName: "Grinding Stone" },
+    electricMotorCapacity: { fieldName: "electricMotorCapacity", uiName: "Electric Motor Capacity" },
+    pulleySize: { fieldName: "pulleySize", uiName: "Pulley Size" },
+    pulleyKeySize: { fieldName: "pulleyKeySize", uiName: "Pulley Key Size" },
+    bearingSizeCover: { fieldName: "bearingSizeCover", uiName: "Bearing Size - Cover" },
+    bearingSizeBracket: { fieldName: "bearingSizeBracket", uiName: "Bearing Size - Bracket" },
+    shaftSize: { fieldName: "shaftSize", uiName: "Shaft Size" },
+    shaftKeySlotSize: { fieldName: "shaftKeySlotSize", uiName: "Shaft Key Slot Size" },
+    grindingCapacity: { fieldName: "grindingCapacity", uiName: "Grinding Capacity Per Hour (on 230 v)" },
+}
+
+export const FlourmillDSPAndDSSP = {
+    name: { fieldName: "name", uiName: "Name" },
+    grindingStone: { fieldName: "grindingStone", uiName: "Grinding Stone" },
+    electricMotorCapacity: { fieldName: "electricMotorCapacity", uiName: "Electric Motor Capacity" },
+    flourmillPulleySize: { fieldName: "flourmillPulleySize", uiName: "Flourmill Pulley" },
+    motorPulleySize: { fieldName: "motorPulleySize", uiName: "Motor Pulley" },
+    bearingSize: { fieldName: "bearingSize", uiName: "Bearing Size" },
+    shaftSize: { fieldName: "shaftSize", uiName: "Shaft Size" },
+    grindingCapacity: { fieldName: "grindingCapacity", uiName: "Grinding Capacity Per Hour (on 230 v)" },
+ }
